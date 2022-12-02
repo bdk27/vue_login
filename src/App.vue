8fb6869cf4d1d55dea8180093e937996c0a37b37
BDK27 <briansam195@gmail.com>
2022-12-02 21:24:28 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="hero">
+    <Login ></Login>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Login from './components/Login'
+import isHide from './assets/js/script'
+
+import { onMounted } from 'vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Login,
+  },
+  setup() {
+
+    onMounted(() => {
+      isHide
+    })
+            
+    
+    return {
+      
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100;300;400;500;700;900&display=swap');
+
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: 'Noto Sans TC', sans-serif;
+    outline: none;
+    border: none;
+    text-decoration: none;
+  }
+  html {
+    font-size: 62.5%;
+  }
+  section {
+    padding: 3rem 2rem;
+  }
+  .hero {
+    width: 100%;
+    min-height: 100vh;
+    background:#efdcd6;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+ 
 </style>
---
+++ src/components/Login.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="test1" id="test1" v-show="gHide">  
+    </div>
+    <div class="test2" id="test2" v-show="rHide">  
+    </div>
+    <div class="btn">
+        <button id="showHidden" @click="isHidden">按我</button>
+    </div>
+   
+</template>
+
+<script>
+    import { ref } from 'vue'
+    export default {
+        name: 'LogIn',
+        setup() {
+            let gHide = ref(true);
+            let rHide = ref(false);
+
+            function show(obj) {
+                if(obj.value === true) {
+                    obj.value = false;
+                }else {
+                    obj.value = true;
+                }
+            }
+            function isHidden() {
+                show(gHide);
+                show(rHide);
+            }
+
+            return {
+                gHide,
+                rHide,
+                isHidden
+            }
+            
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .test1{
+        width: 1000px;
+        height: 500px;
+        background-color: green;
+    }
+    .test2{
+        width: 1000px;
+        height: 500px;
+        background-color: red;
+    }
+    .btn {
+        top: 50%;
+        left: 50px;
+    }
+    button {
+        padding: 10px;
+        cursor: pointer;
+    }
+
+</style>
