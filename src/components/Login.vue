1837a48dfe0aa9fded8cc26963233cbdbdf3d811
BDK27 <briansam195@gmail.com>
2022-12-03 17:28:01 +0800

    add box appearance

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,12 +1,48 @@
 <template>
-    <div class="test1" id="test1" v-show="gHide">  
-    </div>
-    <div class="test2" id="test2" v-show="rHide">  
-    </div>
-    <div class="btn">
-        <button id="showHidden" @click="isHidden">按我</button>
-    </div>
-   
+        <div class="login" v-show="gHide"> 
+            <div class="login-left">
+                <h1>123</h1>
+                <div class="btn">
+                    <button @click="isHidden">按我1</button>
+                </div>
+                <div class="loose-leaf">
+                    <div class="circle"></div>
+                    <div class="circle"></div>
+                    <div class="circle"></div>
+                    <div class="circle"></div>
+                    <div class="circle"></div>
+                    <div class="circle"></div>
+                    <div class="circle"></div>
+                    <div class="circle"></div>
+                </div>
+            </div>
+            <div class="login-right">
+                <h1>456</h1>    
+            </div> 
+        </div>
+
+        <div class="create" v-show="rHide">
+            <div class="create-left">
+                <h1>789</h1>
+            </div>
+            <div class="create-right">
+                <h1>987</h1>
+                <div class="btn">
+                    <button @click="isHidden">按我1</button>
+                </div>
+                <div class="loose-leaf">
+                    <div class="circle"></div>
+                    <div class="circle"></div>
+                    <div class="circle"></div>
+                    <div class="circle"></div>
+                    <div class="circle"></div>
+                    <div class="circle"></div>
+                    <div class="circle"></div>
+                    <div class="circle"></div>
+                </div>
+            </div>   
+        </div>
+
 </template>
 
 <script>
@@ -17,17 +53,12 @@
             let gHide = ref(true);
             let rHide = ref(false);
 
-            function show(obj) {
-                if(obj.value === true) {
-                    obj.value = false;
-                }else {
-                    obj.value = true;
-                }
-            }
             function isHidden() {
-                show(gHide);
-                show(rHide);
+                gHide.value = !gHide.value;
+                rHide.value = !rHide.value;
             }
+
+
 
             return {
                 gHide,
@@ -40,23 +71,100 @@
 </script>
 
 <style lang="scss" scoped>
-    .test1{
-        width: 1000px;
-        height: 500px;
-        background-color: green;
+    $mainColor: #f3ebe9;
+    $darkColor: #efdcd6;
+    $shadow: 0 .5rem 1rem rgba(0, 0, 0, .1);
+    $leftBorder: 2rem 0 0 2rem;
+    $rightBorder: 0 2rem 2rem 0;
+
+    %box {
+        position: absolute;
+        top: 10%;
+        bottom: 10%;
+        left: 15%;
+        right: 15%;
+        background-color: #fff;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        box-shadow: $shadow;
+        border-radius: 2rem;
     }
-    .test2{
-        width: 1000px;
-        height: 500px;
-        background-color: red;
+    %loose-leaf {
+        position: absolute;
+        top: 10rem;
+        display: flex;
+        align-items: center;
+        flex-direction: column;  
     }
+    %circle {
+        width: 3rem;
+        height: 3rem;
+        background-color: #fff;
+        border-radius: 50%;
+        margin-bottom: 5rem
+    }
+    
+    
+    .login {
+        @extend %box;
+        .login-left {
+            position: relative;
+            width: 50%;
+            height: 100%;
+            background-color: $mainColor;
+            border-radius: 2rem 10rem 10rem 2rem;
+            .loose-leaf {
+                @extend %loose-leaf;
+                right: 1rem;
+                .circle {
+                    @extend  %circle
+                }
+            }
+        }
+        .login-right {
+            width: 50%;
+            height: 100%;
+            background-color: #fff;
+            border-radius: $rightBorder;
+        }
+
+    }
+
+    .create{
+        @extend %box;
+        .create-left {
+            width: 50%;
+            height: 100%;
+            background-color: #fff;
+            border-radius: $leftBorder;
+        }
+        .create-right {
+            position: relative;
+            width: 50%;
+            height: 100%;
+            background-color: $mainColor;
+            border-radius: 10rem 2rem 2rem 10rem;
+            .loose-leaf {
+                @extend %loose-leaf;
+                left: 1rem;
+                .circle {
+                    @extend  %circle
+                }
+            }
+        }
+    }
+
     .btn {
         top: 50%;
         left: 50px;
     }
+
     button {
         padding: 10px;
         cursor: pointer;
     }
+    
+  
 
 </style>
