064a7392de1b95d75c9e60ea07158e3c8dcce410
BDK27 <briansam195@gmail.com>
2022-12-04 11:59:16 +0800

    finish login-left

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,27 +1,27 @@
 <template>
-        <div class="login" v-show="gHide"> 
+        <div class="login" v-show="showLogin"> 
             <div class="login-left">
-                <h1>123</h1>
-                <div class="btn">
-                    <button @click="isHidden">按我1</button>
+                <div class="login-content">
+                    <div class="text">
+                        <h1>歡迎回來!</h1>
+                        <p>準備好與我們開始旅程嗎? 探索世界上好玩的事物及人群<br>馬上登入來分享您的趣事</p>
+                    </div>
+                    <div class="image">
+                        <img src="../assets/img/business,-entrepreneurship-and-growth.png">
+                    </div>
+                    <div class="btn">
+                        <span>還沒有帳戶嗎? 點擊按鈕立即註冊</span>
+                        <button @click="isHidden">註冊帳戶</button>
+                    </div>
                 </div>
-                <div class="loose-leaf">
-                    <div class="circle"></div>
-                    <div class="circle"></div>
-                    <div class="circle"></div>
-                    <div class="circle"></div>
-                    <div class="circle"></div>
-                    <div class="circle"></div>
-                    <div class="circle"></div>
-                    <div class="circle"></div>
-                </div>
+
             </div>
             <div class="login-right">
                 <h1>456</h1>    
             </div> 
         </div>
 
-        <div class="create" v-show="rHide">
+        <div class="create" v-show="showCreate">
             <div class="create-left">
                 <h1>789</h1>
             </div>
@@ -50,19 +50,20 @@
     export default {
         name: 'LogIn',
         setup() {
-            let gHide = ref(true);
-            let rHide = ref(false);
+
+            let showLogin = ref(true);
+            let showCreate = ref(false);
 
             function isHidden() {
-                gHide.value = !gHide.value;
-                rHide.value = !rHide.value;
+                showLogin.value = !showLogin.value;
+                showCreate.value = !showCreate.value;
             }
 
 
 
             return {
-                gHide,
-                rHide,
+                showLogin,
+                showCreate,
                 isHidden
             }
             
@@ -73,9 +74,9 @@
 <style lang="scss" scoped>
     $mainColor: #f3ebe9;
     $darkColor: #efdcd6;
+    $darkText: #4d3900;
+    $lightText: #be9c79;
     $shadow: 0 .5rem 1rem rgba(0, 0, 0, .1);
-    $leftBorder: 2rem 0 0 2rem;
-    $rightBorder: 0 2rem 2rem 0;
 
     %box {
         position: absolute;
@@ -90,45 +91,70 @@
         box-shadow: $shadow;
         border-radius: 2rem;
     }
-    %loose-leaf {
-        position: absolute;
-        top: 10rem;
-        display: flex;
-        align-items: center;
-        flex-direction: column;  
-    }
-    %circle {
-        width: 3rem;
-        height: 3rem;
-        background-color: #fff;
-        border-radius: 50%;
-        margin-bottom: 5rem
-    }
-    
     
     .login {
         @extend %box;
         .login-left {
-            position: relative;
             width: 50%;
             height: 100%;
-            background-color: $mainColor;
-            border-radius: 2rem 10rem 10rem 2rem;
-            .loose-leaf {
-                @extend %loose-leaf;
-                right: 1rem;
-                .circle {
-                    @extend  %circle
+            border-radius: 2rem;
+            padding: 1.5rem;
+            .login-content {
+                height: 100%;
+                display: flex;
+                align-items: center;
+                flex-direction: column;
+                background-color: $mainColor;
+                border-radius: 2rem;
+                .text {
+                    text-align: center;
+                    margin-top: 5rem;
+                    h1 {
+                        font-size: 3rem;
+                        padding: 1rem;
+                        color: $darkText;
+                    }
+                    p {
+                        font-size: 1.6rem;
+                        color: $lightText;
+                        line-height: 1.6em;
+                    }
+                }
+                .image {
+                    width: 50rem;
+                    padding: 1rem;
+                    img {
+                        width: 100%;
+                    }
+                }
+                .btn {
+                    span {
+                        font-size: 1.4rem;
+                        color: $lightText;
+                        padding: 1.5rem;
+                    }
+                    button {
+                        border: 1px solid $darkText;
+                        border-radius: 1rem;
+                        background-color: transparent;
+                        color: $darkText;
+                        font-size: 1.6rem;
+                    }
+                    button:hover {
+                        background-color: $darkText;
+                        color: #fff;
+                        transition: .5s;
+                    }
                 }
             }
+            
         }
         .login-right {
             width: 50%;
             height: 100%;
             background-color: #fff;
-            border-radius: $rightBorder;
+            border-radius: 2rem;
         }
-
     }
 
     .create{
@@ -137,21 +163,15 @@
             width: 50%;
             height: 100%;
             background-color: #fff;
-            border-radius: $leftBorder;
+            border-radius: 2rem;
         }
         .create-right {
             position: relative;
             width: 50%;
             height: 100%;
             background-color: $mainColor;
-            border-radius: 10rem 2rem 2rem 10rem;
-            .loose-leaf {
-                @extend %loose-leaf;
-                left: 1rem;
-                .circle {
-                    @extend  %circle
-                }
-            }
+            border-radius: 2rem;
+            
         }
     }
 
